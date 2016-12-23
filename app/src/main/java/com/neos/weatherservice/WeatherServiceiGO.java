5a12a1bb003d8386a2e17059ee6bc1babdcc4603
Neoss75 <neoss7520@gmail.com>
2016-12-24 01:40:51 +0200

    Update

--- app/src/main/java/com/neos/weatherservice/WeatherServiceiGO.java
+++ app/src/main/java/com/neos/weatherservice/WeatherServiceiGO.java
@@ -283,7 +283,6 @@ public class WeatherServiceiGO extends Service {
             return weather;
         }
 
-
         @Override
         protected void onPostExecute(Weather weather) {
             super.onPostExecute(weather);
